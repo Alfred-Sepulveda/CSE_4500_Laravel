035911ab89db6b3ea94e5780249d642876a2d6a5
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 13:11:26 -0800

    debugging create_todos_table

--- database/migrations/2022_03_09_000000_create_events_table.php
+++ database/migrations/2022_03_09_000000_create_events_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps('start_at');
             $table->timestamps('end_at');
             $table->timestamps();
