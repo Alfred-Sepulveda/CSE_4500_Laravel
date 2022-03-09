c617d685abb4343d1ec8d386d35178c3a96cd550
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 15:42:42 -0800

    debugged create.blade

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -10,8 +10,8 @@
 <form method="post" action="{{ route('events.store') }}" >
     @csrf
     <x-adminlte-input name="title" label="Title" />
-    <x-adminlte-input name="start_at" type="number" min=0 max=100 label="Start" />
-    <x-adminlte-input name="end_at" type="number" min=0 max=100 label="End" />
+    <x-adminlte-input name="start_at" type="datetime-local" label="Start" />
+    <x-adminlte-input name="end_at" type="datetime-local" label="End" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
