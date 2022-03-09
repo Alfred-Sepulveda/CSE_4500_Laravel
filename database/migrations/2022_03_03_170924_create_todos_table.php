7adce1c58eb389cc4c974d2f8aaa071487520319
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 10:42:16 -0800

    debugging create_todos_table

--- database/migrations/2022_03_03_170924_create_todos_table.php
+++ database/migrations/2022_03_03_170924_create_todos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->tinyInteger(100);
+            $table->tinyInteger('progress');
             $table->timestamps();
         });
     }
