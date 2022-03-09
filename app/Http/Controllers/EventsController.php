2833b543b117c67c7ac9e5d953d7f1b58ec4e4fa
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 13:45:14 -0800

    added Eventscontroller.php

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Events;
+
+class EventsController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        return view('events.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required',
+            'start_at' => 'required',
+            'end_at' => 'required',
+        ]);
+        $events = Events::create([
+            'title' => $request->title,
+            'start_at' => $request->start_at,
+            'end_at' => $request->end_at,
+        ]);
+        return $this->index();
+
+    }
+
+
+    public function show($id)
+    {
+        $events= Events::find($id);
+        return view('Events.show',compact('events'));
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
