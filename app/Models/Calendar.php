5570f3f73bc83084545ee57d0357afac40b008d4
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-10 11:14:33 -0800

    changed folder from events to calendar

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
+class Calendar extends Model
 {
     protected $fillable = ['title'];
     protected $dates = ['start_at',
