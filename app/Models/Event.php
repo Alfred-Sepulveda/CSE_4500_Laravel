2fa64713e601ac8dcdd83ad08867b7f21522ea86
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 17:11:23 -0800

    looking for error

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    $events = Event::select('title', 'start_at AS start', 'end_at AS end')->get();
     protected $fillable = ['title'];
     protected $dates = ['start_at'];
     protected $dates = ['end_at'];
