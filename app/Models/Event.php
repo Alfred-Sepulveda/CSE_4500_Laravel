22cc3b22b41b77229f3d703e8ee9269cecee6122
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 16:32:58 -0800

    looking for error

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Events extends Model
+class Event extends Model
 {
     protected $fillable = ['title','start_at','end_at'];
     use HasFactory;
