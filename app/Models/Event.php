a7827a70a8d7770b134097cd6c2cb4d9bca4c36e
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-10 12:20:05 -0800

    edited event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = ['title'];
-    protected $dateFormat = 'U';
+    protected $dateFormat = ['start_at',
+                                'end_at'];
 
     //use HasFactory;
 }
