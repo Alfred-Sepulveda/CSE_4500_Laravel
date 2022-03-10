8fa894f635797cf9551eaf7b8cfcb98f528f0552
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-10 11:20:36 -0800

    changed folder from events to calendar

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,7 +10,7 @@ class EventController extends Controller
 
     public function index()
     {
-        $events = Event::select('title', 'start_at AS start', 'end_at AS end')->get();
+        $calendar = Event::select('title', 'start_at AS start', 'end_at AS end')->get();
         return view('calendar', compact('calendar'));
     }
 
