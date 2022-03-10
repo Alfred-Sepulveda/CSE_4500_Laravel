acb44fb6787e4fb38235bb6aed13ebc8b9e19af8
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-10 12:45:50 -0800

    edited eventcontroller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -31,8 +31,8 @@ class EventController extends Controller
 
         $event = Event::create([
             'title' => $request->title,
-            'start_at' => $request->start_at('Y-m-d H:i:s'),
-            'end_at' => $request->end_at('Y-m-d H:i:s'),
+            'start_at' => $request->start('m-d-Y H:i:s'),
+            'end_at' => $request->end('m-d-Y H:i:s'),
         ]);
 
         return $this->index();
