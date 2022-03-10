6c2abba34ab876fa89b178ce3922e77c9a364ef9
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-09 16:55:57 -0800

    looking for error

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $fillable = ['title','start_at','end_at'];
+    protected $fillable = ['title'];
+    protected $dates = ['start_at'];
+    protected $dates = ['end_at'];
+    {{ $user->from_date->format('d/m/Y')}}
     use HasFactory;
 }
--- database/migrations/2022_03_09_000000_create_events_table.php
+++ database/migrations/2022_03_09_000000_create_events_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
+            $table->id();
             $table->string('title');
             $table->timestamp('start_at', $precision = 0);
             $table->timestamp('end_at', $precision = 0);
