e34f2c9e8cb5d918825540b1c4f1ba571f88b2b6
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-11 21:21:58 -0800

    edited eventcontroller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,7 +11,7 @@ class EventController extends Controller
     public function index()
     {
         //$event = Event::select('title', 'start_at AS start', 'end_at AS end')->get();
-        $event = Event::all();
+        $events = Event::all();
         return   view('calendar', compact('calendar'));
         //$todos = Todo::all();
         //return view('todos', compact('todos'));
